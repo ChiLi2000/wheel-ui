75f7a56c80c0721be8b13ddc08e3fc95e5b507a1
chili <1032569304@qq.com>
2020-11-14 20:24:25 +0800

    响应式的Topnav

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,6 +5,7 @@
                 <li>菜单1</li>
                 <li>菜单2</li>
             </ul>
+            <span class="toggleAside"></span>
         </div>
 </template>
 <script lang="ts">
@@ -27,9 +28,21 @@ export default {
        padding: 16px;
        position: relative;
        z-index: 10;
+       justify-content: center;
+       align-items: center;
        > .logo{
            max-width: 6em;
            margin-right: auto;
+       }
+       > .toggleAside{
+           display: none;
+           width: 24px;
+           height: 24px;
+           background: teal;
+           position: absolute;
+           left:16px;
+           top:50%;
+           transform: translateY(-50%);
        }
        > .menu{
            display: flex;
@@ -37,6 +50,17 @@ export default {
            flex-wrap: nowrap;
            >li{
                margin: 0 1em;
+           }
+       }
+       @media(max-width: 500px){
+           > .menu{
+               display: none;
+           }
+           > .logo{
+               margin:0 auto;
+           }
+           > .toggleAside{
+               display: inline-block;
            }
        }
     }
