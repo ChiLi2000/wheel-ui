b4e6b39a689a8eb339c51a8620cc6f1fef8209ba
chili <1032569304@qq.com>
2020-11-14 18:03:06 +0800

    封装Topnav组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
-        <div class="topnav">
-            <div class="logo">logo</div>
-            <ul class="menu">
-                <li>菜单1</li>
-                <li>菜单2</li>
-            </ul>
-        </div>
+        <Topnav/>
         <div class="banner">
             <h1>轮子UI</h1>
             <h2>一个很哇塞的 UI 框架</h2>
@@ -17,22 +11,11 @@
         </div>
     </div>
 </template>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+    components:{Topnav}
+}
+</script>
 <style lang="scss" scoped>
-    .topnav{
-       background: gray;
-       display: flex;
-       padding: 16px;
-       > .logo{
-           max-width: 6em;
-           margin-right: auto;
-       }
-       > .menu{
-           display: flex;
-           white-space: nowrap;
-           flex-wrap: nowrap;
-           >li{
-               margin: 0 1em;
-           }
-       }
-    }
 </style>
