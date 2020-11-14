7f83576b4c3aa3c66f3869b7b5394e3c728fb88d
chili <1032569304@qq.com>
2020-11-14 16:17:04 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>hi
+ 
+</div>
+ <chili/>
+</template>
+
+<script>
+import chili from './components/chili.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    chili
+  }
+}
+</script>
