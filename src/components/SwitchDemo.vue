6f1550546012537e58e64862d50dc3efe1abe7e8
chili <1032569304@qq.com>
2020-11-15 12:51:38 +0800

    使用 props 外部传值

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <Switch>
+        <Switch :value="bool" @change="bool = $event">
     </div>
 </template>
 <script lang="ts">
+import { ref } from 'vue'
 import Switch from '../lib/Switch.vue'
 export default {
-    components:{Switch}
+    components:{Switch},
+    setup(){
+        const bool = ref(false)
+        return {bool}
+    }
 }
 </script>
