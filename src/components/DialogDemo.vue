ce4cd4dbf1d2a9742c9ce7a4e596051d9e653d1d
chili <1032569304@qq.com>
2020-11-16 10:49:38 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-       dialog
-
-    </div>
+    <div>Dialog示例</div>
+    <h2>示例一</h2>
+    <Dialog></Dialog>
 </template>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+    components:{
+        Dialog
+    }
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wraper">
+        <header>标题</header>
+        <main>
+            <p>第一行</p>
+            <p>第二行</p>
+        </main>
+        <footer>
+            <Button>OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+    components:{
+        Button
+    }
+}
+</script>
