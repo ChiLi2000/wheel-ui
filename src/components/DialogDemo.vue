8464ecf3849fa144cdd00b7310e591e86d39df81
chili <1032569304@qq.com>
2020-11-16 15:15:55 +0800

    第二种方法打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,7 +1,6 @@
 <template>
     <div>Dialog示例</div>
     <h2>示例一</h2>
-    <div style="position: relative; z-index: 1;">
         <Button @click="toggle">toggle</Button>
         <Dialog v-model:visible="x"  
         :closeOnClickOverlay="false"
@@ -15,13 +14,14 @@
                 <div>hi2</div>
             </template>    
         </Dialog>
-    </div>
-    <div style="position: relative; z-index: 2; width: 300px; height: 300px; background: red;"></div>
+    <h2>示例二</h2>
+    <Button @click="showDialog">showDialog</Button>
 </template>
 <script lang="ts">
-import { ref } from 'vue'
+import { ref ,h} from 'vue'
 import Button from '../lib/Button.vue'
 import Dialog from '../lib/Dialog.vue'
+import {openDialog} from '../lib/openDialog'
 export default {
     components:{
         Button,
@@ -38,7 +38,20 @@ export default {
         const f2 = () => {
             
         }
-        return {x,toggle,f1,f2}
+        const showDialog = ()=>{
+            openDialog({
+                title:h('strong',{},'标题'),
+                content:'你好',
+                closeOnClickOverlay:'true',
+                 ok() {
+                    console.log('ok')
+                },
+                cancel() {
+                    console.log('cancel')
+                }
+            })
+        }
+        return {x,toggle,f1,f2,showDialog}
     }
 }
 </script>
