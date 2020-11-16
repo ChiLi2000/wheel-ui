25414d988b29e5b5e359440a3fe4f8bb0d684f69
chili <1032569304@qq.com>
2020-11-16 19:26:26 +0800

    渲染 Tabs 内容

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Tabs组件
+         <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+         <component v-for="(c,index) in defaults" :is="c" :key="index" />
+    </div>
+</template>
+
+<script lang='ts'>
+import Tab from './Tab.vue'
+export default {
+    setup(prors,context){
+        const defaults = context.slots.default()
+        const titles = defaults.map((tag)=>{
+            return tag.props.title
+        })
+        return {defaults,titles}
+    }
+}
+</script>
