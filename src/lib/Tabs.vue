609e9940185603915aaa45b1f26108f8e21da4cb
chili <1032569304@qq.com>
2020-11-17 11:16:34 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,19 +4,19 @@
             <div class="wheel-tabs-nav-item"
              v-for="(t,index) in titles" :key="index"
              @click="select(t)" :class="{selected: t===selected}"
-             :ref="el => { if (el) navItems[index] = el }"
+             :ref="el => { if (t===selected) selectedItem = el}"
              >{{t}}</div>
             <div class="wheel-tabs-nav-indicator" ref="indicator"></div>
         </div>
-         <div class="wheel-tabs-content">
-             <component :is="current" :key="current.props.title"/>
-         </div>
+        <div class="wheel-tabs-content">
+            <component :is="current" :key="current.props.title"/>
+        </div>
     </div>
 </template>
 
 <script lang='ts'>
 import Tab from './Tab.vue'
-import {computed, ref,onMounted, onUpdated} from 'vue'
+import {computed, ref,onMounted, watchEffect, onUpdated} from 'vue'
 export default {
     props:{
         selected:{
@@ -24,24 +24,20 @@ export default {
         }
     },
     setup(props,context){
-        const defaults = context.slots.default()
-        const navItems = ref <HTMLDivElement[]>([])
+        const selectedItem = ref<HTMLDivElement>(null)
         const container = ref<HTMLDivElement>(null)
         const indicator = ref<HTMLDivElement>(null)
-        const x =()=>{
-            const divs = navItems.value
-            const result = divs.filter(div=>
-            div.classList.contains('selected'))[0]
-            const {width} = result.getBoundingClientRect()
+        const x=()=>{
+            const {width} = selectedItem.value.getBoundingClientRect()
             indicator.value.style.width = width+'px'
             const {left:left1} = container.value.getBoundingClientRect()
-            const {left:left2} = result.getBoundingClientRect()
+            const {left:left2} = selectedItem.value.getBoundingClientRect()
             const left = left2-left1
             indicator.value.style.left = left+'px'
         }
         onMounted(x)
         onUpdated(x)
-        
+        const defaults = context.slots.default()
         defaults.forEach((tag)=>{
             if(tag.type !== Tab){
                 throw new Error('Tabs 子标签必须是 Tab')          
@@ -56,7 +52,7 @@ export default {
         const current = computed(()=>{
             return defaults.find(tag=>tag.props.title === props.selected)
         })
-        return {defaults,titles,select,current,navItems,container,indicator}
+        return {defaults,titles,select,current,selectedItem,container,indicator}
     }
 }
 </script>
