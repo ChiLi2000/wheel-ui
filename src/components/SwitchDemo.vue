b9364dd2458566ad384ee140958e980196648774
chili <1032569304@qq.com>
2020-11-19 20:41:24 +0800

    抽离需要拷贝的源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch v-model:value="bool"/>
+        <Switch1Demo/>
       </div>
       <div class="demo-actions">
         <Botton>查看代码</Botton>
@@ -16,7 +16,7 @@
   <div class="demo">
     <h2>支持 disabled </h2>
     <div class="demo-component">
-      <Switch v-model:value="bool" disabled />
+      <Switch2Demo/>
     </div>
     <div class="demo-actions">
       <Button>查看代码</Button>
@@ -32,8 +32,10 @@
 import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
 import Button from '../lib/Button.vue'
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
 export default {
-  components: { Switch },
+  components: { Switch1Demo,Switch2Demo },
   setup() {
     const bool = ref(false);
     return { bool };
