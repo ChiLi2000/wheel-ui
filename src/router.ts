742268b7d4d9f68bf42635b705db6afaa27edb8e
chili <1032569304@qq.com>
2020-11-19 19:27:25 +0800

    抽离页面，动态引入 import

--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,9 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue' 
-
+import {h} from 'vue'
+import Markdown from './components/Markdown.vue';
+const md =filename => h(Markdown,{path:`../markdown/${filename}.md`,key:filename})
 const history = createWebHashHistory()
 export const router = createRouter({
     history:history,
@@ -20,9 +19,9 @@ export const router = createRouter({
             component:Doc,
             children:[
                 {path:"",component:DocDemo},
-                {path:"intro",component:Intro},
-                {path:"get-started",component:GetStarted},
-                {path:"install",component:Install},
+                {path:"intro",component:md('intro')},
+                {path:"get-started",component:md('get-started')},
+                {path:"install",component:md('install')},
                 {path:"switch",component:SwitchDemo},
                 {path:"button",component:ButtonDemo},
                 {path:"dialog",component:DialogDemo},
