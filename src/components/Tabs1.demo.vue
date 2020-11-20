db39882a406ba7988060a0baa16c3902c729ff47
chili <1032569304@qq.com>
2020-11-20 17:08:01 +0800

    更新 TabsDemo

---
+++ src/components/Tabs1.demo.vue
@@ -0,0 +1,23 @@
+<demo>常规用法</demo>
+<template>
+   <Tabs v-model:selected="x">
+        <Tab title="导航111111">内容1</Tab>
+        <Tab title="导航">内容2</Tab>
+    </Tabs>
+</template>
+<script lang='ts'>
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import { ref } from 'vue'
+export default {
+    components:{
+        Tabs,Tab
+    },
+    setup(){
+        const x=ref('导航111111')
+        return {
+            x
+        }
+    }
+}
+</script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,26 +1,17 @@
 <template>
-    <div>Tabs 示例</div>
-    <h2>示例1</h2>
-    <hr/>
-    <Tabs v-model:selected="x">
-        <Tab title="导航111111">内容1</Tab>
-        <Tab title="导航">内容2</Tab>
-    </Tabs>
+    <h1>Tabs 示例</h1>
+    <Demo :component="Tab1Demo"/>
 </template>
 
 <script lang='ts'>
-import Tabs from '../lib/Tabs.vue'
-import Tab from '../lib/Tab.vue'
-import { ref } from 'vue'
+import Tab1Demo from './Tabs1.demo.vue'
+import Demo from './Demo.vue'
 export default {
     components:{
-        Tabs,Tab
+        Demo
     },
     setup(){
-        const x=ref('导航111111')
-        return {
-            x
-        }
+        return {Tab1Demo}
     }
 }
 </script>
