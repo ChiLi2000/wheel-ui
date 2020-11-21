fa6cbdfe300e06a2333bf4206a44dffd0755824c
chili <1032569304@qq.com>
2020-11-21 10:41:33 +0800

    build 后页面 bug

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -7,17 +7,10 @@ import { ref } from 'vue'
 
 export default {
   props:{
-      path:{
+      content:{
           type:String,
           require:true
       }
-  },
-  setup(props){
-      const content = ref<string>(null)
-      import(props.path).then(result =>{
-            content.value = result.default
-      })
-      return {content}
   }
 }
 </script>
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,16 @@
 import {createWebHashHistory,createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
-import DocDemo from './components/DocDemo.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import {h} from 'vue'
-import Markdown from './components/Markdown.vue';
-const md =filename => h(Markdown,{path:`../markdown/${filename}.md`,key:filename})
+import Markdown from './components/Markdown.vue'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
+import intro from './markdown/intro.md'
+const md =string => h(Markdown,{content:string,key:string})
 const history = createWebHashHistory()
 export const router = createRouter({
     history:history,
@@ -19,9 +21,9 @@ export const router = createRouter({
             component:Doc,
             children:[
                 {path:"",redirect:'/doc/intro'},
-                {path:"intro",component:md('intro')},
-                {path:"get-started",component:md('get-started')},
-                {path:"install",component:md('install')},
+                {path:"intro",component:md(intro)},
+                {path:"get-started",component:md(getStarted)},
+                {path:"install",component:md(install)},
                 {path:"switch",component:SwitchDemo},
                 {path:"button",component:ButtonDemo},
                 {path:"dialog",component:DialogDemo},
