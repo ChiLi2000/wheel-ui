588d0df0226e3867eeaf72bccf463e00e4be3795
chili <1032569304@qq.com>
2020-11-22 10:16:44 +0800

    界面优化1

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,93 +1,86 @@
 <template>
-    <div class="topnav">
-            <router-link to="/" class="logo">
-                <svg class="icon" >
-                    <use xlink:href="#icon-sheep"></use>
-                </svg>
-            </router-link>
-            <ul class="menu">
-                <li>
-                    <router-link to="/doc">文档</router-link>
-                </li>
-            </ul>
-            <svg 
-            v-if="toggleMenuButtonVisible" 
-            class="toggleAside" 
-            @click="toggleMenu"
-            >
-                <use xlink:href="#icon-menu"></use>
-            </svg>
-        </div>
+  <div class="topnav">
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
+    <router-link to="/" class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-sheep"></use>
+      </svg>
+    </router-link>
+    <ul class="menu">
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from 'vue'
+import { inject, Ref } from "vue";
 export default {
-    props:{
-        toggleMenuButtonVisible:{
-            type:Boolean,
-            default:false
-        }
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false,
     },
-    setup(){
-        const menuVisible = inject<Ref<boolean>>('menuVisible')
-        const toggleMenu = ()=>{
-            menuVisible.value = !menuVisible.value
-        }
-        return{toggleMenu}
-    }
-    
-}
+  },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    const toggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    return { toggleMenu };
+  },
+};
 </script>
 <style lang="scss" scoped>
-    .topnav{
-        color:#8a4aba;
-       display: flex;
-       padding: 16px;
-       position: fixed;
-       top:0;
-       left:0;
-       width:100%;
-       z-index: 20;
-       justify-content: center;
-       align-items: center;
-       > .logo{
-           max-width: 6em;
-           margin-right: auto;
-           > svg{
-               width: 32px;
-               height: 32px;
-           }
-       }
-       > svg{
-               width: 32px;
-               height: 32px;
-           }
-       > .toggleAside{
-           display: none;
-           position: absolute;
-           left:16px;
-           top:50%;
-           transform: translateY(-50%);
-        //    background: fade-out(black, 0.9);
-       }
-       > .menu{
-           display: flex;
-           white-space: nowrap;
-           flex-wrap: nowrap;
-           >li{
-               margin: 0 1em;
-           }
-       }
-       @media(max-width: 500px){
-           > .menu{
-               display: none;
-           }
-           > .logo{
-               margin:0 auto;
-           }
-           > .toggleAside{
-               display: inline-block;
-           }
-       }
+.topnav {
+  color: #8a4aba;
+  display: flex;
+  padding: 16px 36px;
+  width: 100%;
+  z-index: 20;
+  justify-content: center;
+  align-items: center;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+    > svg {
+      width: 32px;
+      height: 32px;
     }
+  }
+  > .toggleAside {
+    display: none;
+  }
+  > svg {
+    width: 32px;
+    height: 32px;
+  }
+  > .toggleAside {
+    display: none;
+    position: fixed;
+    top: 18px;
+    left: 16px;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin-right: 32px;
+    }
+    > .toggleAside {
+      display: inline-block;
+    }
+  }
+}
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,112 +1,124 @@
 <template>
-    <div class="layout">
-      <Topnav :toggleMenuButtonVisible="true" class="nav"/>
-        <div class="content">
-            <aside v-if="menuVisible">
-                <h2>文档</h2>
-                <ol>
-                    <li>
-                        <router-link to="/doc/intro">介绍</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/install">安装</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/get-started">开始使用</router-link>
-                    </li>
-                </ol>
-                <h2>组件列表</h2>
-                <ol>
-                    <li>
-                        <router-link to="/doc/switch">Switch 组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/button">Button 组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/dialog">Dialog 组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/tabs">Tabs 组件</router-link>
-                    </li>
-                </ol>
-            </aside>
-            <main>
-                <router-view/>
-            </main>
-        </div>
+  <div class="layout">
+    <Topnav :toggleMenuButtonVisible="true" class="nav" />
+    <div class="content">
+      <aside v-if="menuVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始使用</router-link>
+          </li>
+        </ol>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>
+        <router-view />
+      </main>
     </div>
+  </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from 'vue'
-import Topnav from '../components/Topnav.vue'
+import { inject, Ref } from "vue";
+import Topnav from "../components/Topnav.vue";
 export default {
-    components:{Topnav},
-    setup(){
-        const menuVisible = inject<Ref<boolean>>('menuVisible')
-        return{menuVisible}
-    }
-}
+  components: { Topnav },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    return { menuVisible };
+  },
+};
 </script>
 <style lang="scss" scoped>
-$aside-index:10;
+$aside-index: 10;
 
-.layout{
-    display: flex;flex-direction: column;
-    height: 100vh;
-    > .nav{
-        flex-shrink: 0;
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 10px 0px;
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    width: 100%;
+    overflow: auto;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
-    > .content{
-        flex-grow: 1;
-        padding-top: 60px;
-        padding-left: 156px;
-        @media(max-width: 500px){
-            padding-left: 0;
+  }
+}
+.content {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding-left: 16px;
+    background: white;
+    overflow: auto;
+  }
+}
+
+aside {
+  border-right: 1px solid rgba(0, 0, 0, 0.15);
+  height: 100%;
+  z-index: $aside-index;
+  > h2 {
+    margin: 16px 0;
+    padding-left: 36px;
+  }
+  > ol {
+    > li {
+      > a {
+        display: block;
+        padding: 8px 126px 8px 36px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: #ebd2ff;
+        border-right: 3px solid #8a4aba;
+        color: #8a4aba;
+      }
+      &:hover {
+        color: #8a4aba;
+      }
+    }
+  }
+  @media (max-width: 500px) {
+    > h2 {
+      padding-left: 16px;
+    }
+    > ol {
+      > li {
+        > a {
+          padding: 8px 126px 8px 16px;
         }
+      }
     }
+  }
 }
-.content{
-    display: flex;
-    > aside{
-        flex-shrink: 0;
-    }
-    > main{
-        flex-grow: 1;
-        padding: 16px;
-        background: white;
-    }
-}
-
-aside{
-    background:#ebd2ff;
-    width:150px;
-    padding: 16px 0;
-    position: fixed;
-    top:0;
-    left:0;
-    padding-top: 70px;
-    height: 100%;
-    z-index: $aside-index;
-    >h2{
-        margin-bottom: 4px;
-        padding:0 16px;
-    }
-    >ol{
-        >li{
-            >a{
-                display: block;
-                padding:4px 16px;
-                text-decoration: none;
-            }
-            .router-link-active{
-                background: white;
-}
-        } 
-    }
-   main{
-       overflow: auto;
-   }
-}
-    
 </style>
