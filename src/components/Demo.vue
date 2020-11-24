f025128068f4fda50579181516b1ea02df65a20c
chili <1032569304@qq.com>
2020-11-24 20:21:05 +0800

    查看与隐藏代码 css

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -4,12 +4,26 @@
     <div class="demo-component">
       <component :is="component" />
     </div>
-    <div class="demo-actions">
-      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
-      <Button @click="toggleCode" v-else>查看代码</Button>
-    </div>
+
     <div class="demo-code" v-if="codeVisible">
       <pre class="language-css" v-html="html" />
+    </div>
+    <div class="demo-actions">
+      <div @click="hideCode" v-if="codeVisible" class="commonCode">
+        <svg class="icon">
+          <use xlink:href="#icon-up"></use>
+        </svg>
+        <span>隐藏代码</span>
+      </div>
+      <div @click="toggleCode" v-else class="commonCode">
+        <svg class="icon">
+          <use xlink:href="#icon-down"></use>
+        </svg>
+        <span>查看代码</span>
+      </div>
+
+      <!-- <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="toggleCode" v-else>查看代码</Button> -->
     </div>
   </div>
 </template>
@@ -43,10 +57,10 @@ export default {
 
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
+$color: #8a4aba;
 
 %item {
   padding: 8px 16px;
-  border-top: 1px dashed $border-color;
 }
 .demo {
   border: 1px solid $border-color;
@@ -63,11 +77,46 @@ $border-color: #d9d9d9;
   }
 
   &-actions {
-    @extend %item;
+    > .commonCode {
+      height: 44px;
+      margin-top: -1px;
+      cursor: pointer;
+      border-top: 1px dashed $border-color;
+      position: relative;
+      text-align: center;
+      left: 0;
+      color: $color;
+
+      > svg {
+        width: 16px;
+        height: 44px;
+        fill: #d3dce6;
+        transition: 500ms;
+        text-align: center;
+      }
+      > span {
+        opacity: 0;
+        position: absolute;
+        font-size: 14px;
+        line-height: 44px;
+        transition: 500ms;
+      }
+    }
+  }
+  &-actions > .commonCode:hover {
+    span {
+      opacity: 1;
+      transform: translateX(-30px);
+    }
+    > svg {
+      fill: $color;
+      transform: translateX(-40px);
+    }
   }
 
   &-code {
     @extend %item;
+    transition: all 3s;
 
     > pre {
       line-height: 1.1;
